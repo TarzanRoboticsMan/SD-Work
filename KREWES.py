b5d4e5a6e74c98d3c29824c1dcbd34497dfa8d12
TarzanRoboticsMan <dsontag00@stuy.edu>
2019-09-10 08:40:20 -0400

    Wrote up Jude code

---
+++ KREWES.py
@@ -0,0 +1,6 @@
+KREWES = {
+        'orpheus': ['Emily', 'Kevin', 'Vishwaa', 'Eric', 'ray', 'Jesse', 'Tiffany', 'Amanda', 'Junhee', 'Jackie', 'Tyler', 'Emory', 'Ivan']
+        }
+
+team = input("What team? ")
+print(KREWES[team][int(random(KREWES[team]))])
