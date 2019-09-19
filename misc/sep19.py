163d097ef0b49a30f067c8e48476bf7df1f7b132
TarzanRoboticsMan <dsontag00@stuy.edu>
2019-09-19 09:27:21 -0400

    whatever

--- misc/sep19.py
+++ misc/sep19.py
@@ -2,10 +2,17 @@ from flask import Flask
 
 app = Flask(__name__) #create instance of class Flask
 
-@app.route("/") #assign following fxn to run when run route requested
+@app.route('/') #assign following fxn to run when run route requested
 def hello_word():
     print(__name__) #where will this go?
-    return "No hablo queso!"
+    return "Love 19th"
+
+coll = [0,1,1,2,3,5,8]
+@app.route("/my_foist_template")
+def test_templ():
+    print(__name__)
+    return render
+
 if __name__ == "__main__":
     app.debug = True
     app.run()
