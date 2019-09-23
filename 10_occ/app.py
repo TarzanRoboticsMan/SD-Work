d6b8e20517b9f9026324e549d0f58d8913308b26
TarzanRoboticsMan <dsontag00@stuy.edu>
2019-09-23 09:16:52 -0400

    ethan is smarter than me

--- 10_occ/app.py
+++ 10_occ/app.py
@@ -1,14 +1,43 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, random
+
+fn = 'occupations.csv'
+fileR = open(fn, 'r')
+
+jobs = fileR.readlines()
+occupations = dict()
+
+for job in jobs:
+    words = job.split(',')
+    percentage = words.pop(len(words) - 1)
+    #delimiter = ','
+    occupations[','.join(words)] = float(percentage)
+
+def randomJob(dictOfJobs):
+    perct = dictOfJobs['Total'] * 100
+    randomperct = random.random() * perct
+    keys = dictOfJobs.keys()
+    x = 0;
+    while ((dictOfJobs[keys[x]] * 100) < randomperct):
+        randomperct = randomperct - (dictOfJobs[keys[x]] * 100)
+        x = x + 1
+    return keys[x]
+
+print(randomJob(occupations))
+
 
 app = Flask(__name__) #create instance of class Flask
 
 @app.route('/') #assign following fxn to run when run route requested
 def hello_word():
     print(__name__) #where will this go?
-    return "We love the 19th"
+    return "Default"
 
-@app.route("occupyflaskst")
+@app.route("/occupyflaskst")
+def protest():
 
+    return render_template('model_templt.html',
+                            titl="Occupation List",
+                            head="Returns random occupation from the table below\nMr. Mylky\nCoby Sontag,Ethan Chen")
 if __name__ == "__main__":
     app.debug = True
     app.run()
