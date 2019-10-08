1167ef9bf67bb9a7f7d09bc7dc3c7d7f0beccbab
TarzanRoboticsMan <dsontag00@stuy.edu>
2019-10-08 09:25:51 -0400

    eat database

--- k17_carepkg/db_builder.py
+++ k17_carepkg/db_builder.py
@@ -1,5 +1,5 @@
 #Clyde "Thluffy" Sinclair
-#SoftDev  
+#SoftDev
 #skeleton :: SQLITE3 BASICS
 #Oct 2019
 
@@ -9,20 +9,22 @@ import csv       #facilitate CSV I/O
 
 DB_FILE="discobandit.db"
 
-db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
-c = db.cursor()               #facilitate db ops
+#db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
+#c = db.cursor()               #facilitate db ops
 
 #==========================================================
+
+#command = "CREATE TABLE courses("code" text, "mark" integer, "id" integer);"          # test SQL stmt in sqlite3 shell, save as string
 
 # < < < INSERT YOUR POPULATE-THE-DB CODE HERE > > >
+with open('courses.csv') as csvfile:
+     reader = csv.reader(csvfile)
+     for row in reader:
+         print row;
 
-
-command = ""          # test SQL stmt in sqlite3 shell, save as string
-c.execute(command)    # run SQL statement
+#c.execute(command)    # run SQL statement
 
 #==========================================================
 
-db.commit() #save changes
-db.close()  #close database
-
-
+#db.commit() #save changes
+#db.close()  #close database
