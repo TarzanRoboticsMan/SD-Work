01c63a256821cd63e14bcf50867bf9abf3349bc3
TarzanRoboticsMan <dsontag00@stuy.edu>
2019-10-10 09:23:42 -0400

    YEET

--- k17_carepkg/db_builder.py
+++ k17_carepkg/db_builder.py
@@ -9,22 +9,33 @@ import csv       #facilitate CSV I/O
 
 DB_FILE="discobandit.db"
 
-#db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
-#c = db.cursor()               #facilitate db ops
+db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
+c = db.cursor()               #facilitate db ops
 
 #==========================================================
 
-#command = "CREATE TABLE courses("code" text, "mark" integer, "id" integer);"          # test SQL stmt in sqlite3 shell, save as string
+c.execute("CREATE TABLE IF NOT EXISTS courses(code text, mark integer, id integer)")
+# test SQL stmt in sqlite3 shell, save as string
 
 # < < < INSERT YOUR POPULATE-THE-DB CODE HERE > > >
 with open('courses.csv') as csvfile:
-     reader = csv.reader(csvfile)
+     reader = csv.reader(csvfile, delimiter=',')
      for row in reader:
-         print row;
-
-#c.execute(command)    # run SQL statement
+         #print(row)
+         command = "INSERT INTO courses VALUES ('"+row[0]+"','"+row[1]+"','"+row[2]+"')"
+         c.execute(command)
 
 #==========================================================
 
-#db.commit() #save changes
-#db.close()  #close database
+c.execute("CREATE TABLE IF NOT EXISTS students (name TEXT, age INTEGER, id INTEGER)")
+
+with open('students.csv') as csvfile:
+     reader = csv.reader(csvfile)
+     for row in reader:
+         command = "INSERT INTO courses VALUES ('"+row[0]+"','"+row[1]+"','"+row[2]+"')"
+         c.execute(command)
+
+#==========================================================
+
+db.commit() #save changes
+db.close()  #close database
