cb3c20ea40ec07a1bc382f9a8856ef7712834177
Coby Sontag <TarzanRoboticsMan@users.noreply.github.com>
2019-10-10 15:56:52 -0400

    We finished

--- 18_db-nmcrnch/stu_mean.py
+++ 18_db-nmcrnch/stu_mean.py
@@ -15,23 +15,29 @@ c = db.cursor()               #facilitate db ops
 #==========================================================
 
 #Create new table
-c.execute("CREATE TABLE IF NOT EXISTS stu_avg(id text, mark real)")
+c.execute("CREATE TABLE IF NOT EXISTS stu_avg(id text, avg real)")
 
 #Find each student
-print(c.execute("SELECT id FROM students"))
-'''
-with open('courses.csv') as csvfile:
-     reader = csv.reader(csvfile, delimiter=',')
-     for row in reader:
-         #print(row)
-         command = "INSERT INTO courses VALUES ('"+row[0]+"','"+row[1]+"','"+row[2]+"')"
-         c.execute(command)
+kidList = c.execute("SELECT name, id FROM students")
+courseList = c.execute("SELECT mark, id FROM courses")
 
-Compute each student’s average
-Display each student’s name, id, and average
-Create a table of IDs and associated averages, named "stu_avg"
-Facilitate adding rows to the courses table
-'''
+gradeTot = 0
+numClasses = 0
+
+for kid in kidList: #For each student
+         for class in courseList: #Check each course
+                  if class[id] == kid[id]: #To see if it's their grade
+                           numClasses++ 
+                           gradeTot+=class[mark] #Add to average
+         c.execute("INSERT INTO stu_avg VALUES("+kid+","+gradeTot/numClasses)
+         print(kid[name]+", id "+kid[id]+": average "+gradeTot/numClasses)
+         gradeTot = 0
+         numClasses = 0 #reset counters
+         
+def addCourse(code, mark, id):
+         c.execute("INSERT INTO courses VALUES("+code+","+mark+","+id+")")
+
+
 #==========================================================
 
 db.commit() #save changes
